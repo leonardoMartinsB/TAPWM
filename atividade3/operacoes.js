051adc47acab2f51295f6c7a8c60d981f872549f
Leonardo Martins Brasilio <139197874+leonardoMartinsB@users.noreply.github.com>
2026-04-08 15:20:47 -0300

    Add files via upload

---
+++ atividade3/operacoes.js
@@ -0,0 +1,8 @@
+let n1 = parseFloat(prompt("Digite o primeiro número:"));
+let n2 = parseFloat(prompt("Digite o segundo número:"));
+
+alert("Soma: " + (n1 + n2));
+alert("Subtração: " + (n1 - n2));
+alert("Produto: " + (n1 * n2));
+alert("Divisão: " + (n1 / n2));
+alert("Resto: " + (n1 % n2));
